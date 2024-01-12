4ff76194b2c9f1a48861ca43cfc6da707a6f351d
Nathuz <nathuznoepupin@gmail.com>
2024-01-12 22:02:21 +0100

    🐛 Footer auto preload

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   build: {
+    modulePreload: false,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
