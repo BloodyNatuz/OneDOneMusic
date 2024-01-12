71b8b0ab9695f311d26d0411000b407a0c26725d
Nathuz <nathuznoepupin@gmail.com>
2024-01-12 20:06:11 +0100

    🏗️ Footer and Nav into a folder

---
+++ scripts/layout/navbar.js
@@ -0,0 +1,34 @@
+// Sélection du body
+let body = document.querySelector("body");
+
+// Création et importation de la navbar
+let navbar = document.createElement("navbar");
+body.appendChild(navbar);
+
+// Création et importation des nav-links
+let firstNavLink = document.createElement("a");
+firstNavLink.className = "nav-link";
+firstNavLink.href ="/";
+let imgLogo = document.createElement("img");
+imgLogo.src = "https://one-day-one-music.nathuz.fr/assets/odom-white-wpGiodzV.svg"
+
+let secondNavLink = document.createElement("a");
+secondNavLink.className = "nav-link";
+secondNavLink.href ="#";
+secondNavLink.textContent = "Musique du jour";
+
+let thirdNavLink = document.createElement("a");
+thirdNavLink.className = "nav-link";
+thirdNavLink.href ="#";
+thirdNavLink.textContent = "Historique";
+
+let fourthNavLink = document.createElement("a");
+fourthNavLink.className = "nav-link";
+fourthNavLink.href ="/contact/contact.html";
+fourthNavLink.textContent = "Contact";
+
+navbar.appendChild(firstNavLink);
+firstNavLink.appendChild(imgLogo);
+navbar.appendChild(secondNavLink);
+navbar.appendChild(thirdNavLink);
+navbar.appendChild(fourthNavLink);
