2d53526b3ff758e62d8e150399371cb67ea6fc9f
Nathuz <nathuznoepupin@gmail.com>
2024-01-12 22:46:31 +0100

    🐛 Footer preloading for mysterious reason

--- scripts/alanding.js
+++ scripts/alanding.js
@@ -13,15 +13,15 @@ title.textContent = "One Day One Music";
 main.appendChild(title);
 
 // Création et importation de la description
-// let description = document.createElement("p");
-// description.className = "description";
-// description.textContent =
-//   'Bienvenue sur One Day One Music. Chaque jour, une nouvelle musique apparaitra sur le site, donnez votre avis dessus en cliquant sur "J\'aime" ou "Je n\'aime pas".';
-// main.appendChild(description);
+let description = document.createElement("p");
+description.className = "description";
+description.textContent =
+  'Bienvenue sur One Day One Music. Chaque jour, une nouvelle musique apparaitra sur le site, donnez votre avis dessus en cliquant sur "J\'aime" ou "Je n\'aime pas".';
+main.appendChild(description);
 
-// // Création et importation du bouton
-// let homeBtn = document.createElement("a");
-// homeBtn.className = "primary-btn";
-// homeBtn.href = "/music/music.html";
-// homeBtn.textContent = "Découvrir →";
-// main.appendChild(homeBtn);
+// Création et importation du bouton
+let homeBtn = document.createElement("a");
+homeBtn.className = "primary-btn";
+homeBtn.href = "/music/music.html";
+homeBtn.textContent = "Découvrir →";
+main.appendChild(homeBtn);
