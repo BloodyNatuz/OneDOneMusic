ee6f55f2e97d31bf51cddd5add22ee16362a0a58
Nathuz <nathuznoepupin@gmail.com>
2024-01-12 21:56:45 +0100

    🐛 Footer auto preload

--- main.js
+++ main.js
@@ -2,9 +2,4 @@ import "./style/main.scss";
 import "./scripts/components/bg-anim";
 import "./scripts/layout/navbar";
 import "./scripts/pages/landing";
-import "./scripts/layout/footer";
-
-var cls = document.querySelectorAll('link[rel="modulepreload"]');
-for(var i = 0; i < cls.length; i++) {
-   cls[i].removeAttribute('rel');
-}
+import "./scripts/footer";
