86dc821be4267593905a1962aca033fca1f3f4c3
Nathuz <nathuznoepupin@gmail.com>
2024-01-12 21:22:24 +0100

    🐛 Fix footer loading

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style/main.scss";
 import "./scripts/components/bg-anim";
 import "./scripts/layout/navbar";
-import "./scripts/pages/landing";
+import "./scripts/landing";
 
 import "./scripts/layout/footer";
