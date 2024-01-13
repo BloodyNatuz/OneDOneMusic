956caff4166e68802da4e40936c1d8dbacdf2415
Nathuz <nathuznoepupin@gmail.com>
2024-01-13 14:08:24 +0100

    🔥 Favicon import

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 import "./style/main.scss";
-import favicon from "./scripts/config/favicon";
 import bgAnim from "./scripts/components/bg-anim";
 import navbar from "./scripts/layout/navbar";
 import pageContent from "./scripts/pages/landing";
 import footer from "./scripts/layout/footer";
 
 function loadScript(){
-    favicon();
     bgAnim();
     navbar();
     pageContent();
