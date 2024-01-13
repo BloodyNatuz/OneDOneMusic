611794b7da30e950e98dd6eeff81c892a4728db7
Nathuz <nathuznoepupin@gmail.com>
2024-01-13 13:28:31 +0100

    🐛 Footer preloading for mysterious reason

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 import "./style/main.scss";
-
 import "./scripts/components/bg-anim";
+import navbar from "./scripts/layout/navbar";
+import pageContent from "./scripts/pages/landing";
+import footer from "./scripts/layout/footer";
 
-import "./scripts/layout/navbar";
 
-import "./scripts/alanding";
+function loadScript(){
+    navbar();
+    pageContent();
+    footer();
+}
 
-import "./scripts/footer";
+loadScript();
--- scripts/layout/navbar.js
+++ scripts/layout/navbar.js
@@ -1,34 +1,36 @@
-// Sélection du body
-let body = document.querySelector("body");
+export default function navbar(){
+  // Sélection du body
+  let body = document.querySelector("body");
 
-// Création et importation de la navbar
-let navbar = document.createElement("navbar");
-body.appendChild(navbar);
+  // Création et importation de la navbar
+  let navbar = document.createElement("navbar");
+  body.appendChild(navbar);
 
-// Création et importation des nav-links
-let firstNavLink = document.createElement("a");
-firstNavLink.className = "nav-link";
-firstNavLink.href = "/";
-let imgLogo = document.createElement("img");
-imgLogo.src ="";
+  // Création et importation des nav-links
+  let firstNavLink = document.createElement("a");
+  firstNavLink.className = "nav-link";
+  firstNavLink.href = "/";
+  let imgLogo = document.createElement("img");
+  imgLogo.src ="";
 
-let secondNavLink = document.createElement("a");
-secondNavLink.className = "nav-link";
-secondNavLink.href = "/music/music.html";
-secondNavLink.textContent = "Musique du jour";
+  let secondNavLink = document.createElement("a");
+  secondNavLink.className = "nav-link";
+  secondNavLink.href = "/music/music.html";
+  secondNavLink.textContent = "Musique du jour";
 
-let thirdNavLink = document.createElement("a");
-thirdNavLink.className = "nav-link";
-thirdNavLink.href = "#";
-thirdNavLink.textContent = "Historique";
+  let thirdNavLink = document.createElement("a");
+  thirdNavLink.className = "nav-link";
+  thirdNavLink.href = "#";
+  thirdNavLink.textContent = "Historique";
 
-let fourthNavLink = document.createElement("a");
-fourthNavLink.className = "nav-link";
-fourthNavLink.href = "/contact/contact.html";
-fourthNavLink.textContent = "Contact";
+  let fourthNavLink = document.createElement("a");
+  fourthNavLink.className = "nav-link";
+  fourthNavLink.href = "/contact/contact.html";
+  fourthNavLink.textContent = "Contact";
 
-navbar.appendChild(firstNavLink);
-firstNavLink.appendChild(imgLogo);
-navbar.appendChild(secondNavLink);
-navbar.appendChild(thirdNavLink);
-navbar.appendChild(fourthNavLink);
+  navbar.appendChild(firstNavLink);
+  firstNavLink.appendChild(imgLogo);
+  navbar.appendChild(secondNavLink);
+  navbar.appendChild(thirdNavLink);
+  navbar.appendChild(fourthNavLink);
+}
