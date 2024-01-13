047b0bc214b24573a7957213c9e01ca4d1451487
Nathuz <nathuznoepupin@gmail.com>
2024-01-13 13:59:46 +0100

    🔨 Import favicon script

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import "./style/main.scss";
+import favicon from "./scripts/config/favicon";
 import bgAnim from "./scripts/components/bg-anim";
 import navbar from "./scripts/layout/navbar";
 import pageContent from "./scripts/pages/landing";
 import footer from "./scripts/layout/footer";
 
-import imgLogo from "./img/odom-white.svg"
-
 function loadScript(){
+    favicon();
     bgAnim();
     navbar();
     pageContent();
@@ -14,11 +14,3 @@ function loadScript(){
 }
 
 loadScript();
-
-// Favicon script
-let head = document.querySelector("head");
-let linkFavicon = document.createElement("link");
-linkFavicon.rel = "icon";
-linkFavicon.type = "image/svg+xml";
-linkFavicon.href = imgLogo;
-head.appendChild(linkFavicon);
