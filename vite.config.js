63e2b0da4b52a7fc46ad8ce96c10ec4eba43c8db
Nathuz <nathuznoepupin@gmail.com>
2024-01-13 17:44:52 +0100

    🔧 Server cors

--- vite.config.js
+++ vite.config.js
@@ -22,4 +22,13 @@ export default defineConfig({
       },
     ],
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.deezer.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/api/, '')
+      }
+    }
+  }
 });
